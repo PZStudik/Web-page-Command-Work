8c31680a16611ce249cb047f2c7b7bf6c4b10bed
Kobliuk_Nazar <nazar.kobliuk.pz.2023@lpnu.ua>
2024-11-18 06:47:51 +0200

    Card Nazar.K + position - fix

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,15 +3,17 @@ import React, { useState } from "react";
 const Body = () => {
 
     // Масив з нашими картками. Тут задається зображення картки, підпис та інформація для InfoSection
+    // До своєї картки ставите ID згідно номеру в групі:Єва-1, Таміла-2, Дмитро-3, Назар-4, Уляна-5, Влад-6
+
     const cards = [
         {
             // Картка екземпляру
-            id: 1,
-            img: "/avatar-default.png", // Шлях до зображення
-            name: "John D",             // Ім'я картки
-            info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed mi sit amet magna posuere finibus. Suspendisse nibh augue, commodo in" // Інформація про картку
-                + " \u25AA Location: _____. \u25AA Number: _____. \u25AA Email: _____. " // Контактна інформація
-
+            id: 4,
+            img: "/Nazar_Kobliuk.jpg",     // Шлях до зображення
+            name: "Kobliuk Nazar",                // Ім'я картки
+            position: "Software Engineer",  // Посада в картці
+            info: "I am a Software Engineering student at LPNU with experience in C, C++, and Assembly programming. I am also familiar with Python and Pascal and have minor experience in HTML/CSS for creating simple web pages. I enjoy solving problems, learning new technologies, and working on both system-level and web development projects." // Інформація про картку
+                + " \u25AA Location: Lviv. \u25AA Number: +380664118658. \u25AA Email: nazar.kobliuk.pz.2023@lpnu.ua" // Контактна інформація
         }
     ];
 
@@ -57,14 +59,20 @@ const Body = () => {
                         key={card.id}
                         style={{
                             ...styles.card,
-                            ...(selectedCard?.id === card.id ? styles.selectedCard : {}), // Динамічне виділення вибраної картки
+                            ...(selectedCard?.id === card.id
+                                ? {
+                                    ...styles.selectedCard,
+                                    border: card.id === 4 ? "2px solid #68001D" : "2px solid #111111",
+                                    boxShadow: card.id === 4 ? "0 4px 50px rgba(140, 0, 140, 0.9)" : "0 4px 50px rgba(110, 0, 110, 0.6)"
+                                  }
+                                : {}),
                         }}
                         onClick={() => handleCardClick(card)} // Обробник кліку
                     >
                         <img src={card.img} alt={card.name} style={styles.photo} />
                         <div style={styles.textContainer}>
                             <h2 style={styles.name}>{card.name}</h2>
-                            <p style={styles.position}>Position</p>
+                            <p style={styles.position}>{ card.position}</p>
                         </div>
                     </div>
                 ))}
@@ -110,7 +118,8 @@ const styles = {
     photo: {
         width: "100px",
         height: "100px",
-        borderRadius: "50%",
+        borderRadius: "10%",
+        border: "2px solid #111111",
         objectFit: "cover",
     },
 
